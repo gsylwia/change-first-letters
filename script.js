37619cc31f33d4e82164aa84257807f05ad8aa4d
Sylwia <g.sylwia@vp.pl>
2022-10-11 22:35:52 +0200

    fix code

--- script.js
+++ script.js
@@ -9,10 +9,11 @@ const changeFirstLetters = (text) => {
     	if (el[0] === el[0].toLowerCase() ) {
             const upper = el[0].toUpperCase() + el.substring(1);
             arrSentence.push(upper);
-        } else {
-			const lower = el[0].toLowerCase() + el.substring(1);
-            arrSentence.push(lower);
+            return;
         }
+
+        const lower = el[0].toLowerCase() + el.substring(1);
+        arrSentence.push(lower);
     });
     
     const newSentence = arrSentence.join(" ");
